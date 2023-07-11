3cb52d21b6d0e98fae337fab9751fd2ae60f6698
surendraLongre <murari.longre2366@gmail.com>
2023-07-11 19:41:01 +0530

    make the command work for more than just one file also chane bright_cyan to bright_yellow

--- gaussian_max_step.sh
+++ gaussian_max_step.sh
@@ -3,45 +3,52 @@
 #colors
 red=$(tput setaf 1)
 green=$(tput setaf 2)
-bright_cyan=$(tput setaf 14)
+bright_yellow=$(tput setaf 11)
 bold=$(tput bold)
 nc_red=$(tput sgr0)
 nc='\033[0m' # reset color
 blue=$(tput setaf 4)
-output_file="$(echo $1 | cut -d '.' -f1)_max_count.txt"
 
+#write a for loop for more than one file
+
+	if [ $# -gt 0 ];
+	then
+
+	for file in "$@";
+	do
 #check whether file exist or not
-if ! [ -e $1 ];
-then
-echo "$1 file does not exist"
-exit
-fi
+	if ! [[ -e $file && $(echo $file | cut -d '.' -f2) = 'log' ]];
+	then
+	echo "${red}$file does not exist or file extension is not .log${nc_red}"
+	exit
+	fi
 
 #check if the process has already normally terminated
-#tail -1 "$1" | awk '{print $1}' - | if [ "$(cat -)" = 'Normal' ]; then echo "${bright_cyan}The process has Normally terminated${nc_red}"; exit 0; fi
-result=$(tail -1 "$1" | awk '{print $1}' -)
-if [ "$result" = 'Normal' ]; then
-  echo "${bright_cyan}Normally terminated${nc_red}"
-  exit 0
-fi
+#tail -1 "$file" | awk '{print $file}' - | if [ "$(cat -)" = 'Normal' ]; then echo "${bright_yellow}The process has Normally terminated${nc_red}"; exit 0; fi
+	result=$(tail -1 "$file" | awk '{print $1}' -)
+	output_file="$(echo $file | cut -d '.' -f1)_max_count.txt"
+	if [ "$result" = 'Normal' ]; then
+	echo "${bright_yellow}Normally terminated${nc_red}"
+	continue
+	fi
 
-
-
-if ! [ -e $output_file ];
-then
-touch $output_file;
-echo "no previous data" > "$output_file"
-
-fi
-
+#step3: check if file does not exist
+	if ! [ -e $output_file ];
+	then
+	touch $output_file;
+	echo "no previous data" > "$output_file"
+	fi
 
 #output
 
+	echo "${bright_yellow} $file${nc_red}"
+	echo " $( head -1 $output_file) --> $(wc -l $file | awk '{print $1}')" 
+	tail -1 $output_file
+	echo -e " ..."
+	echo "$(wc -l $file | awk '{print $1}')" > $output_file
+	echo "$(grep maximum $file | tail -1)" >> $output_file
+	tail -1 $output_file
+	echo
 
-echo " $( head -1 $output_file) --> $(wc -l $1 | awk '{print $1}')" 
-echo
-tail -1 $output_file
-echo -e " ..."
-echo "$(wc -l $1 | awk '{print $1}')" > $output_file
-echo "$(grep maximum $1 | tail -1)" >> $output_file
-tail -1 $output_file
+	done
+	fi
