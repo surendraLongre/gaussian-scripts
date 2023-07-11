c359d5c48aa7651b521bcfabb979a7d8ca1fdf7e
surendraLongre <murari.longre2366@gmail.com>
2023-07-11 10:27:22 +0530

    check if the file has already been terminated normally

--- gaussian_max_step.sh
+++ gaussian_max_step.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+#colors
+red=$(tput setaf 1)
+green=$(tput setaf 2)
+bold=$(tput bold)
+nc_red=$(tput sgr0)
+nc='\033[0m' # reset color
+blue=$(tput setaf 4)
 output_file="$(echo $1 | cut -d '.' -f1)_max_count.txt"
 
 #check whether file exist or not
@@ -8,6 +15,17 @@ then
 echo "$1 file does not exist"
 exit
 fi
+
+#check if the process has already normally terminated
+#tail -1 "$1" | awk '{print $1}' - | if [ "$(cat -)" = 'Normal' ]; then echo "${bold}${green}The process has Normally terminated${nc_red}"; exit 0; fi
+result=$(tail -1 "$1" | awk '{print $1}' -)
+if [ "$result" = 'Normal' ]; then
+  echo
+  echo "${bold}${green}Normally terminated${nc_red}"
+  exit 0
+fi
+
+
 
 if ! [ -e $output_file ];
 then
